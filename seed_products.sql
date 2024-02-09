0de7479fb6eb45e07801fe1ce9f32ec25cdb39b6
Ricardo <ricardoalbornozgil@live.com>
2024-02-08 20:49:25 -0500

    Initial Commit

---
+++ seed_products.sql
@@ -0,0 +1,14 @@
+DROP database IF EXISTS products_db;
+
+CREATE database products_db;
+
+\c products_db
+
+CREATE TABLE products
+(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    price FLOAT CHECK (price > 0),
+    can_be_returned BOOLEAN NOT NULL
+)
+
